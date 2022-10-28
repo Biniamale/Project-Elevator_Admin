1b3af722c4c76832df0bf3e0c291e1aba24a3e84
Bobby Sonhjos <robert.jodelsohn@student.kyh.se>
2022-10-28 10:26:04 +0200

    Created Elevator Feature
    Added folder and index page.

---
+++ Project-Elevator_Admin/Pages/Elevators/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_Elevator_Admin.Pages.Elevators
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
